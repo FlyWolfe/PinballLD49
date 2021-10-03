0aea1fe6f8aea365b2120ba51ed0fe37d45e3a7b
FlyWolfe <mjsherry248@gmail.com>
2021-10-02 22:10:43 -0400

    Removed debug log that I forgot to remove

--- Assets/Scripts/Player/Player.cs
+++ Assets/Scripts/Player/Player.cs
@@ -102,7 +102,6 @@ public class Player : MonoBehaviour
     void SlingBall(Vector2 direction, float magnitude)
     {
         Vector3 forceDirection = Quaternion.Euler(board.eulerAngles.x, 0, 0) * (Vector3)direction;
-        Debug.LogError("Force Direction: " + forceDirection);
         
         rigidBody.AddForce(forceDirection * magnitude, ForceMode.Impulse);
     }
