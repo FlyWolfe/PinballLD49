727ba3c8d3619e7a9475528355cdcdecc9a53f55
Simon Bernard <simon_bernard@live.de>
2021-10-03 20:49:46 +0200

    Add New Platforms, Adjust Final Map, Adjust Final Map Post Processing, Add Map Root Script To Rotate Map In Place At Start Of The Game

---
+++ Assets/Scripts/GameController/MapRoot.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MapRoot : MonoBehaviour
+{
+    private void Start()
+    {
+        transform.rotation = Quaternion.Euler(45, 0, 0);
+    }
+}
